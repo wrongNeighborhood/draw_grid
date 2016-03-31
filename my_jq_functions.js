fd784f1908a3c40f48ba609356348d09babba792
wrongNeighborhood <sukhov.sasha@gmail.com>
2016-03-31 15:20:38 +0300

    Таблица 8 на 8

--- my_jq_functions.js
+++ my_jq_functions.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-	$('div').click(function(){
+	var content = "<table>"
+	var size = 8;
+	/*var width = ($(document).width())*/;
+	/*var height = Math.floor($(document).height()/8);*/
+	/*$('.brick').css({"width":"85px","height":"85px"});*/
+	for(j=0;j<size;j++)
+	{
+		content += "<tr>"
+		for(i=0; i<size; i++)
+		{
+	    	content += '<td>' + 
+	    	'<div class="brick"><p><br>'+j+' '+i+'</p></div>'
+	    	+'</td>';
+		}
+		content+="</tr>";
+	}
+	content += "</table>"
+	$('.wrapper').append(content);
+	$('.brick').click(function(){
 		$(this).hide();
-	})
+	});
+	
 })
